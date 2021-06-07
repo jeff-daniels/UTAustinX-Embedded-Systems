4c5ef56a91454a6e13049363f42ba6a260d69793
Jeff Daniels <36964220+jeff-daniels@users.noreply.github.com>
2021-06-07 10:40:21 -0400

    write Calc_Area function

--- Lab5_FunctionsInC/main.c
+++ Lab5_FunctionsInC/main.c
@@ -44,8 +44,10 @@ int main (void) {
 //    the width is greater than 20. 
 unsigned long Calc_Area(unsigned long l, unsigned long w) {
   unsigned long result;
-
-// Put your Lab 5 code here
-  
+	if (((l<3)||(l >20))||(w<3)||(w>20)){
+		result = 0;
+	} else {
+		result = l * w;
+	}
   return(result);
 }
