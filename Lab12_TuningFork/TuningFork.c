29195c697025818973f671be8b74cfb04ef17b21
Jeff Daniels <36964220+jeff-daniels@users.noreply.github.com>
2021-07-18 12:15:32 -0400

    use finite state machine

--- Lab12_TuningFork/TuningFork.c
+++ Lab12_TuningFork/TuningFork.c
@@ -33,16 +33,43 @@
  */
 
 
+// ***** Pre-Processor Directives Section *****
 #include "TExaS.h"
 #include "..//tm4c123gh6pm.h"
+
+// ***** Global Declarations Section *****
+#define TUNING_FORK (*(volatile unsigned long *)0x40004010) // PA2
+#define SWITCH  (*(volatile unsigned long *)0x40004020) // PA3
+
+#define Off 0
+#define HiPressed 1
+#define LoPressed 2
+#define HiReleased 3
+#define LoReleased 4
+#define TurnOff 5
+
+// Linked data structure
+struct State {
+	unsigned long Buzzer;
+	unsigned long Next[2];
+};
+typedef const struct State StateType;
+
+// State Transistion Table for operating the tuning fork
+StateType FSM[6] = {
+	{0x00, {Off, HiPressed}},	// Off
+	{0x04, {LoReleased, LoPressed}}, 	// HiPressed
+	{0x00, {HiReleased, HiPressed}}, 	// LoPressed
+	{0x04, {LoReleased, TurnOff}}, 		// HiReleased
+	{0x00, {HiReleased, TurnOff}}, 		// LoReleased
+	{0x00, {Off, TurnOff}}						// TurnOff
+};
 
 
 // basic functions defined at end of startup.s
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void WaitForInterrupt(void);  // low power mode
-
-
 
 
 // input from PA3, output from PA2, SysTick interrupts
@@ -56,24 +83,30 @@ void Sound_Init(void){ unsigned long volatile delay;
   GPIO_PORTA_DR8R_R |= 0x04;    // can drive up to 8mA PA2 output
   GPIO_PORTA_AFSEL_R &= ~0x0C;  // disable alt funct on PA3, PA2
   GPIO_PORTA_DEN_R |= 0x0C;     // enable digital I/O on PA3, PA2
+	GPIO_PORTA_DATA_R &= ~0x04;   // initialize PA2 to off
   NVIC_ST_CTRL_R = 0;           // disable SysTick during setup
   NVIC_ST_RELOAD_R = 90908;     // reload value for 880hz (2 x desired freq) (assuming 80MHz)
   NVIC_ST_CURRENT_R = 0;        // any write to current clears it
   NVIC_SYS_PRI3_R = NVIC_SYS_PRI3_R&0x00FFFFFF; // priority 0                
   NVIC_ST_CTRL_R = 0x00000007;  // enable with core clock and interrupts
-  EnableInterrupts();
-
 }
 
+unsigned long CurrentState;		// index to current state
+unsigned long test;
 // called at 880 Hz
 void SysTick_Handler(void){
-
+	TUNING_FORK = FSM[CurrentState].Buzzer;	// Set PA2 to either high or low
+	CurrentState = FSM[CurrentState].Next[(SWITCH&0x08)>>3];	// PA3 input determines next state
 }
 
+
 int main(void){// activate grader and set system clock to 80 MHz
-  TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
+  DisableInterrupts();	// disable interrupts before initialization
+	TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
   Sound_Init();         
   EnableInterrupts();   // enable after all initialization are done
+	CurrentState = Off;		// initial and typical state
+	
   while(1){
     // main program is free to perform other tasks
     // do not use WaitForInterrupt() here, it may cause the TExaS to crash
