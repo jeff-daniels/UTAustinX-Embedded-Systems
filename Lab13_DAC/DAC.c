38383757db4ad264b67e1c56a1fa98c0e5ebd7b4
Jeff Daniels <36964220+jeff-daniels@users.noreply.github.com>
2021-08-01 15:07:23 -0400

    enable 8mA output

--- Lab13_DAC/DAC.c
+++ Lab13_DAC/DAC.c
@@ -10,18 +10,18 @@
 #include "..//tm4c123gh6pm.h"
 
 // **************DAC_Init*********************
-// Initialize 4-bit DAC 
+// Initialize 6-bit DAC 
 // Input: none
 // Output: none
 void DAC_Init(void){unsigned long volatile delay;
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB; // activate port B
   delay = SYSCTL_RCGC2_R;    // allow time to finish activating
-	//while ((SYSCTL_RCGC2_R & 0x00000010) == 0){};
-  GPIO_PORTB_AMSEL_R &= ~0x0F;      // no analog 
-  GPIO_PORTB_PCTL_R &= ~0x0000FFFF; // regular function
-  GPIO_PORTB_DIR_R |= 0x0F;      // make PB3-0 out
-  GPIO_PORTB_AFSEL_R &= ~0x0F;   // disable alt funct on PB3-0
-  GPIO_PORTB_DEN_R |= 0x0F;      // enable digital I/O on PB3-0
+  GPIO_PORTB_AMSEL_R &= ~0x3F;      // no analog 
+  GPIO_PORTB_PCTL_R &= ~0x00FFFFFF; // regular function
+  GPIO_PORTB_DIR_R |= 0x3F;      // make PB5-0 out
+	GPIO_PORTB_DR8R_R  |= 0x3F;    //    Enable 8 mA drive on PB5 - PB0
+  GPIO_PORTB_AFSEL_R &= ~0x3F;   // disable alt funct on PB5-0
+  GPIO_PORTB_DEN_R |= 0x3F;      // enable digital I/O on PB5-0
 }
 
 
@@ -30,5 +30,5 @@ void DAC_Init(void){unsigned long volatile delay;
 // Input: 4-bit data, 0 to 15 
 // Output: none
 void DAC_Out(unsigned long data){
-  GPIO_PORTB_DATA_R = data;
+  GPIO_PORTB_DATA_R |= data&0x0F;
 }
--- Lab13_DAC/Piano.c
+++ Lab13_DAC/Piano.c
@@ -31,6 +31,13 @@ void Piano_Init(void){volatile unsigned long delay;
 // Output: 0 to 15 depending on keys
 // 0x01 is key 0 pressed, 0x02 is key 1 pressed,
 // 0x04 is key 2 pressed, 0x08 is key 3 pressed
-unsigned long Piano_In(void){
-  return (GPIO_PORTE_DATA_R&0x0F)^0x11; // input
+unsigned long Piano_In(void){unsigned long keyPressed;
+	keyPressed = GPIO_PORTE_DATA_R&0x0F; // input
+
+	if (((keyPressed)&(keyPressed - 1)) == 0){
+		return 0x00;	// multiple keys pressed returns 0
+	} 
+	else{
+		return keyPressed; // input
+	}
 }
